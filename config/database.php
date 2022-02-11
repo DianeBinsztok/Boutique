a925dd18fbac356fa607c3185fca3930ef668ac7
DianeBinsztok <diane.binsztok@le-campus-numerique.fr>
2022-02-11 16:10:02 +0100

    1.1 PDO connexion

--- config/database.php
+++ config/database.php
@@ -1,1 +1,18 @@
 <?php
+include './config/secret.php';
+
+// error management
+// dtb access
+try {
+    $dtb = new PDO('mysql:host=localhost;dbname=Blog', $username, $password);
+    //Blog is the name of the dtb as defined on nginx-Adminer (so "Blog", and not "blog_dump.sql")
+    foreach ($dtb->query('SELECT * from products') as $row) {
+        // test : display my data on rows separated by a <br/>
+        //echo implode("<br/>", $row);
+    }
+    $dbh = null;
+} catch (PDOException $e) {
+    print "Erreur !: " . $e->getMessage() . "<br/>";
+    die();
+}
+
